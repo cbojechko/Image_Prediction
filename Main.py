a46eb7edfe229ea5c09a93fc63bf914f43a260d0
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2022-01-04 07:41:53 -0800

    moving preprocessing into its own folder

--- Main.py
+++ Main.py
@@ -11,10 +11,10 @@ import os
 basepath = os.path.join('.', 'Data', 'Patient')
 
 if True:
-    from SortRTImages import SortRTIMAGE
+    from PreProcessingTools.CreatePDOSAndRIImages import CreatePDOS_and_RI_Images
     # Factor with which to downsample EPID images are 1280x1280
     Ndownsample = 5
-    SortRTIMAGE(basepath, Ndownsample)
+    CreatePDOS_and_RI_Images(basepath, Ndownsample)
 
 RTIpath = os.path.join(basepath,'RTIMAGE')
 CBCTpath = os.path.join(basepath,'CT')
