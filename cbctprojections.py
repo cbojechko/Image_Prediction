a3864f3b391920604a941d88de9cdeb7f2dbba88
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2022-01-04 09:40:44 -0800

    transferred over to a parallel process

--- cbctprojections.py
+++ cbctprojections.py
@@ -3,9 +3,52 @@ import numpy as np
 import pydicom
 import matplotlib.pyplot as plt
 import SimpleITK as sitk
+from tqdm import tqdm
 import glob
 import re
 import rays
+from threading import Thread
+from multiprocessing import cpu_count
+from queue import *
+
+
+def rayvec_worker(A):
+    q, pbar, kwargs = A
+    point_maker = RunRayVec(**kwargs)
+    while True:
+        item = q.get()
+        if item is None:
+            break
+        else:
+            point_maker.new_trace(**item)
+        pbar.update()
+        q.task_done()
+
+
+class RunRayVec(object):
+    def __init__(self, SID, SAD, epidEdgeX, xstep, epidEdgeZ, zstep, image, CTinfo,
+                 rotsource, voxDim, voxSize, rayvec, gantryangle, origin, nz):
+        self.image = image
+        self.CTinfo = CTinfo
+        self.voxDim = voxDim
+        self.voxSize = voxSize
+        self.SID = SID
+        self.SAD = SAD
+        self.epidEdgeX = epidEdgeX
+        self.epidEdgeZ = epidEdgeZ
+        self.xstep, self.zstep = xstep, zstep
+        self.rayvec = rayvec
+        self.gantryangle, self.origin, self.rotsource = gantryangle, origin, rotsource
+        self.nz = nz
+
+    def new_trace(self, kk, ll):
+        PointOnEPID = np.array([(self.epidEdgeX + kk * self.xstep), self.SID - self.SAD,
+                                (self.epidEdgeZ + ll * self.zstep)])
+        ray = rays.EPID_rotate(self.gantryangle, self.origin, PointOnEPID) - self.rotsource
+        # Double check the indexing
+        self.rayvec[self.nz - ll, kk - 1] = rays.new_trace(self.image, self.CTinfo,
+                                                           self.rotsource, ray, self.voxDim,
+                                                           self.voxSize)
 
 
 def MakeCBCTProjection(RIpath,CBCTpath):
@@ -88,20 +131,35 @@ def MakeCBCTProjection(RIpath,CBCTpath):
                 print("Projection File exists")
                 continue
             # # Scan over EPID panel range
-            
+            thread_count = int(cpu_count()*.8)
+            q = Queue(maxsize=thread_count)
+            kwargs = {'SID': SID, 'SAD': SAD, 'epidEdgeX': epidEdgeX,
+                      'epidEdgeZ': epidEdgeZ, 'xstep': xstep, 'zstep': zstep,
+                      'image': image, 'CTinfo': CTinfo, 'rotsource': rotsource,
+                      'voxDim': voxDim, 'voxSize': voxSize, 'rayvec': rayvec,
+                      'gantryangle': gantryang, 'origin': origin, 'nz':nz}
+            pbar = tqdm(total=nx*nz, desc='Making ray trace')
+            A = (q, pbar, kwargs)
+            threads = []
+            for worker in range(thread_count):
+                t = Thread(target=rayvec_worker, args=(A,))
+                t.start()
+                threads.append(t)
             for kk in range(1,nx):
                 for ll in range(1,nz):
-                    PointOnEPID = np.array([(epidEdgeX+kk*xstep),SID-SAD,(epidEdgeZ+ll*zstep)]) 
-                    ray= rays.EPID_rotate(gantryang,origin,PointOnEPID)-rotsource
-                    # Double check the indexing 
-                    rayvec[nz-ll,kk-1] = rays.new_trace(image,CTinfo,rotsource,ray,voxDim,voxSize)
-            
+                    item = {'kk': kk, 'll': ll}
+                    q.put(item)
+            for i in range(thread_count):
+                q.put(None)
+            for t in threads:
+                t.join()
             cbctproj = np.float32(rayvec)
-            projfileout = "cbctprojection" + str(int(fxs[j])) + '_G' + str(int(gantryang)) + "_" + str(int(dates[j]))
+            projfileout = "cbctprojection" + str(int(fxs[j])) + '_G' + str(int(gantryang)) + "_" + str(int(dates[j])) + '_new'
             print("Save npz    " + projfileout)
             arrout = os.path.join(CBCTpath, projfileout)
             #print("Saving Projection "+ str(arrout))
             np.savez_compressed(arrout,cbctproj)
+            return None
 
 def main():
     # Main loop
