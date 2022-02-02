dcb5a3eb50ee6b61717c61aea6dd213a9d5dc58e
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2022-02-01 17:10:30 -0800

    working on making these projections faster

--- PreProcessingTools/CreateDRR.py
+++ PreProcessingTools/CreateDRR.py
@@ -1,7 +1,6 @@
 import sys
 
 import SimpleITK as sitk
-import itk
 import os
 from itk import RTK as rtk
 import itk
@@ -26,18 +25,17 @@ if not os.path.exists(os.path.join(data_path, "Image.mha")):
   #   print(index)
   #   print(date)
   # Loading 3D CT image
-  sitk.WriteImage(dicom_handle, os.path.join(data_path, "Image.mha"))
+  sitk.WriteImage(dicom_handle, os.path.join('.', "Image.mha"))
 else:
-  dicom_handle = sitk.ReadImage(os.path.join(data_path, "Image.mha"))
-dicom_handle.SetDirection((0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0))
-sitk.WriteImage(dicom_handle, "Test.nii.gz")
-CT = itk.imread(os.path.join(data_path, "Image.mha"), pixel_type=itk.F)
+  dicom_handle = sitk.ReadImage(os.path.join('.', "Image.mha"))
+# dicom_handle.SetDirection((0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0))
+# sitk.WriteImage(dicom_handle, "Test.nii.gz")
+CT = itk.imread(os.path.join('.', "Image.mha"), pixel_type=itk.F)
 
 # Defines the image type
 Dimension_CT = 3
 PixelType = itk.F
 ImageType = itk.Image[PixelType, Dimension_CT]
-
 # Create a stack of empty projection images
 print("Got here")
 ConstantImageSourceType = rtk.ConstantImageSource[ImageType]
@@ -45,7 +43,7 @@ constantImageSource = ConstantImageSourceType.New()
 print("And here")
 # Define origin, sizeOutput and spacing (still need to change these)
 origin = dicom_handle.GetOrigin()
-numberOfProjections = 1 # 360
+numberOfProjections = 180 # 360
 dicom_size = dicom_handle.GetSize()
 sizeOutput = [ dicom_size[0], dicom_size[1], numberOfProjections]
 spacing = dicom_handle.GetSpacing()
@@ -59,7 +57,7 @@ constantImageSource.SetConstant(0.)
 geometry = rtk.ThreeDCircularProjectionGeometry.New()
 firstAngle = 0.
 angularArc = 360.
-firstangle = 180.
+# firstangle = 180.
 sid = 600 # source to isocenter distance
 sdd = 1200 # source to detector distance
 
@@ -67,20 +65,17 @@ sdd = 1540  # source to imager distance
 sid = 1000  # source to isocenter
 for x in range(0,numberOfProjections):
   angle = firstAngle + x * angularArc / numberOfProjections
-  geometry.AddProjection(sid,sdd,angle)
-
+  geometry.AddProjection(sid, sdd, 0., 0., 0., 90., angle)
+# sid, ssd, gantryAngle, projOffsetX, projOffsetY, outOfPlaneAngle, inPlaneAngle, sourceOffsetX, sourceOffsetY
 # Writing the geometry to disk
 xmlWriter = rtk.ThreeDCircularProjectionGeometryXMLFileWriter.New()
-xmlWriter.SetFilename ( 'geo.xml' )
-xmlWriter.SetObject ( geometry )
+xmlWriter.SetFilename('geo.xml')
+xmlWriter.SetObject(geometry)
 xmlWriter.WriteFile()
 
 REIType = rtk.JosephForwardProjectionImageFilter[ImageType, ImageType]
 rei = REIType.New()
-semiprincipalaxis = [ 50, 50, 50]
-center = [ 0, 0, 10]
-
-rei.SetGeometry( geometry )
+rei.SetGeometry(geometry)
 rei.SetInput(0, constantImageSource.GetOutput())
 rei.SetInput(1, CT)
 rei.Update()
