dbcb585881037028620c322a6d6f8981e48a613a
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2022-02-03 09:04:37 -0800

    DRR works!

--- PreProcessingTools/CreateDRR.py
+++ PreProcessingTools/CreateDRR.py
@@ -31,114 +31,81 @@ else:
   dicom_handle = sitk.ReadImage(os.path.join('.', "Image.mha"))
 # dicom_handle.SetDirection((0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0))
 # sitk.WriteImage(dicom_handle, "Test.nii.gz")
-InputImageType = itk.Image[itk.F, 3]
+rotations = [0, 0, 0] # x, y, z
 
-CT = itk.imread(os.path.join('.', "Image.mha"), pixel_type=itk.F)
-region = CT.GetBufferedRegion()
+translations = [0, 0, 0] # x, y, z
+
+pixel_iso_center = [0, 0, 0] # x, y, z
+rprojection = 0. # Projection angle in degrees
+sid = 1000 # source to isocenter
+imRes = dicom_handle.GetSpacing()
+
+im_sx = imRes[0]
+im_sy = imRes[1]
+
+imSize = dicom_handle.GetSize()
+dx = imSize[0]
+dy = imSize[1]
+
+o2Dx = 0.
+o2Dy = 0.
+
+threshold = 0
+
+Dimension = 3
+
+InputImageType = itk.Image[itk.F, Dimension]
+OutputImageType = itk.Image[itk.F, Dimension]
+
+
+image = itk.imread(os.path.join('.', "Image.mha"), pixel_type=itk.F)
+origin = image.GetOrigin()
+
+spacing = image.GetSpacing()
+region = image.GetBufferedRegion()
 
 filter = itk.ResampleImageFilter[InputImageType, InputImageType].New()
 filter.SetDefaultPixelValue(0)
-filter.SetInput(CT)
+filter.SetInput(image)
 
 transform = itk.Euler3DTransform[itk.D].New()
 transform.SetComputeZYX(True)
 
 dtr = np.arctan(1)*4/180
-transform.SetTranslation((0, 0, 0))
-transform.SetRotation(dtr*90, 0, 0)
 
-imSize = region.GetSize()
-imOrigin = dicom_handle.GetOrigin()
+transform.SetTranslation(translations)
+transform.SetRotation(rotations[0] * dtr, rotations[1] * dtr, rotations[2] * dtr)
 
 isocenter = [0, 0, 0]
-imRes = dicom_handle.GetSpacing()
-isocenter[0] = imOrigin[0] + imRes[0] * (imSize[0]) / 2.0
-isocenter[1] = imOrigin[1] + imRes[1] * (imSize[1]) / 2.0
-isocenter[2] = imOrigin[2] + imRes[2] * (imSize[2]) / 2.0
-
-transform.SetCenter(imOrigin)
-imRes = dicom_handle.GetSpacing()
-imSize = dicom_handle.GetSize()
-
-o2Dx = (512-1)/2
-o2Dy = (512-1)/2
-origin = [0, 0, 0]
-
-origin[0] = -512 * o2Dx
-origin[1] = -512 * o2Dy
-origin[2] = -1000
-filter.SetOutputOrigin(imOrigin)
+for i in range(3):
+  isocenter[i] = origin[i] + imRes[i] * imSize[i]/2
+transform.SetCenter(isocenter)
 
 interpolator = itk.SiddonJacobsRayCastInterpolateImageFunction[InputImageType, itk.D].New()
-interpolator.SetProjectionAngle(dtr*0)
-interpolator.SetFocalPointToIsocenterDistance(1000)
-interpolator.SetThreshold(0.)
+interpolator.SetProjectionAngle(rprojection*dtr)
+interpolator.SetFocalPointToIsocenterDistance(sid)
+interpolator.SetThreshold(threshold)
 interpolator.SetTransform(transform)
 interpolator.Initialize()
-
 filter.SetInterpolator(interpolator)
 
-dicom_size = dicom_handle.GetSize()
-sizeOutput = [ dicom_size[0], dicom_size[1], 1]
-spacing = dicom_handle.GetSpacing()
-
-filter.SetSize(sizeOutput)
+filter.SetSize([dx, dy, 1])
 filter.SetOutputSpacing(spacing)
+
+o2Dx = (dx-1)/2
+o2Dy = (dy-1)/2
+#origin = [0, 0, 0]
+origin[0] += im_sx * o2Dx
+origin[1] += im_sy * o2Dy
+origin[2] = -sid
+
+filter.SetOutputOrigin(origin)
+filter.SetOutputDirection(image.GetDirection())
 filter.Update()
+
 output = filter.GetOutput()
 
 writer = itk.ImageFileWriter[InputImageType].New()
 writer.SetFileName(os.path.join('.','Output.mha'))
 writer.SetInput(output)
 writer.Update()
-# Defines the image type
-Dimension_CT = 3
-PixelType = itk.F
-ImageType = itk.Image[PixelType, Dimension_CT]
-# Create a stack of empty projection images
-print("Got here")
-ConstantImageSourceType = rtk.ConstantImageSource[ImageType]
-constantImageSource = ConstantImageSourceType.New()
-print("And here")
-# Define origin, sizeOutput and spacing (still need to change these)
-
-numberOfProjections = 8 # 360
-
-
-
-constantImageSource.SetOrigin( origin )
-constantImageSource.SetSpacing( spacing )
-constantImageSource.SetSize( sizeOutput )
-constantImageSource.SetConstant(0.)
-
-# Defines the RTK geometry object
-geometry = rtk.ThreeDCircularProjectionGeometry.New()
-firstAngle = 0.
-angularArc = 360.
-# firstangle = 180.
-sid = 600 # source to isocenter distance
-sdd = 1200 # source to detector distance
-
-sdd = 1540  # source to imager distance
-sid = 1000  # source to isocenter
-for x in range(numberOfProjections):
-  angle = firstAngle + x * angularArc / numberOfProjections
-  geometry.AddProjection(sid, sdd, angle, 0., 0., 90., 0., 0., 0.)
-for x in range(numberOfProjections):
-  angle = firstAngle + x * angularArc / numberOfProjections
-  geometry.AddProjection(sid, sdd, 0., 0., 0., 90., angle, 0., 0.)
-# geometry.AddProjection(sid, sdd, 0, 0., 0., 90., 0., 0., 0.)
-# sid, ssd, gantryAngle, projOffsetX, projOffsetY, outOfPlaneAngle, inPlaneAngle, sourceOffsetX, sourceOffsetY
-# Writing the geometry to disk
-xmlWriter = rtk.ThreeDCircularProjectionGeometryXMLFileWriter.New()
-xmlWriter.SetFilename('geo.xml')
-xmlWriter.SetObject(geometry)
-xmlWriter.WriteFile()
-
-REIType = rtk.JosephForwardProjectionImageFilter[ImageType, ImageType]
-rei = REIType.New()
-rei.SetGeometry(geometry)
-rei.SetInput(0, constantImageSource.GetOutput())
-rei.SetInput(1, CT)
-rei.Update()
-itk.imwrite(rei.GetOutput(), 'stack1.mha')
