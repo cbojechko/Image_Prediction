404306fbaf79a6105eae45e3aea3458ed818a4ce
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2022-04-26 13:56:48 -0700

    code for padding

---
+++ PreProcessingTools/Pad_CBCTs_From_Digital_Phantom.py
@@ -0,0 +1,42 @@
+import SimpleITK as sitk
+import os
+import numpy as np
+from glob import glob
+from PreProcessingTools.Main import array_to_sitk
+from PlotScrollNumpyArrays import plot_scroll_Image
+
+
+def update_CBCT(nifti_path):
+    status_file = os.path.join(nifti_path, "Padded_from_air.txt")
+    if os.path.exists(status_file):
+        return None
+    padded_cbcts = glob(os.path.join(nifti_path, "Padded_CBCT_*"))
+    for padded_cbct in padded_cbcts:
+        padded_handle = sitk.ReadImage(padded_cbct)
+        spacing = padded_handle.GetSpacing()
+        padded_np = sitk.GetArrayFromImage(padded_handle)
+        flattened_padded = np.max(padded_np, axis=(1, 2))
+        air = np.where(flattened_padded == -1000)[0]
+        not_air = np.where(flattened_padded > -1000)[0]
+        if len(air) > 0:
+            if len(not_air) > 0:
+                start = not_air[0] + int(20/spacing[-1])
+                stop = not_air[-1] - int(20/spacing[-1])
+                padded_np[:start] = padded_np[start]
+                padded_np[stop:] = padded_np[stop]
+                new_padded_handle = array_to_sitk(padded_np, reference_handle=padded_handle)
+                sitk.WriteImage(new_padded_handle, padded_cbct)
+        fid = open(status_file, 'w+')
+        fid.close()
+
+
+def main_update_CBCT(path=r'R:\Bojechko\phantom'):
+    phantom_directories = os.listdir(path)
+    for phantom_dir in phantom_directories:
+        print(phantom_dir)
+        full_path = os.path.join(path, phantom_dir)
+        update_CBCT(os.path.join(full_path, 'Niftiis'))
+
+
+if __name__ == '__main__':
+    main_update_CBCT()
