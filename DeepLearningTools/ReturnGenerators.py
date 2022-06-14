5ed9df4623db42b7ccbd38f1392c21098a7d4645
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2022-06-14 08:40:39 -0700

    cleaning the documentation up a bit

--- DeepLearningTools/ReturnGenerators.py
+++ DeepLearningTools/ReturnGenerators.py
@@ -26,108 +26,31 @@ def get_mean_std(train_generator):
     return None
 
 
-def return_generator(records_path, cache, batch=1):
-    global_norm = True
-    add_5cm_keys = True
+def return_generator(records_path, batch=1, proj_to_panel=True, add_5cm_keys=True):
     generator = DataGeneratorClass(record_paths=records_path, delete_old_cache=True)
-    all_keys = ('pdos_array', 'fluence_array','drr_array', '5cm_deep_array', 'iso_array', '5cm_shallow_array')
-    drr_keys = ('drr_array', '5cm_deep_array', 'iso_array', '5cm_shallow_array', )
-    normalize_keys = ('iso_array',)
-    input_keys = ('pdos_array', 'drr_array', 'iso_array')
+    all_keys = ('pdos_array', 'fluence_array','drr_array', 'deep_to_panel_array', 'iso_to_panel_array', 'shallow_to_panel_array')
+    drr_keys = ('drr_array', 'deep_to_panel_array', 'iso_to_panel_array', 'shallow_to_panel_array', )
+    input_keys = ('pdos_array', 'drr_array', 'iso_to_panel_array')
     if add_5cm_keys:
-        input_keys = ('pdos_array', 'drr_array', '5cm_deep_array', 'iso_array', '5cm_shallow_array')
-        normalize_keys = ('5cm_deep_array', 'iso_array', '5cm_shallow_array')
+        if proj_to_panel:
+            input_keys = ('pdos_array', 'drr_array', 'deep_to_panel_array', 'iso_to_panel_array',
+                          'shallow_to_panel_array')
+        else:
+            input_keys = ('pdos_array', 'drr_array', '5cm_deep_array', 'iso_array', 'shallow_array')
     print(f"Inputs are {input_keys}")
     base_processors = [
-                     CProcessors.Squeeze(image_keys=all_keys),
-                     CProcessors.ExpandDimension(axis=-1, image_keys=all_keys),
-                     CProcessors.MultiplyImagesByConstant(keys=drr_keys,
-                                                          values=(1/90, 1/90, 1/90, 1/90)),
-                     ]
-    if global_norm:
-        base_processors += [
-                            CProcessors.MultiplyImagesByConstant(keys=('pdos_array',
-                                                                    'fluence_array'),
-                                                                 values=(1, 1)) #(1/2, 1/(.3876*2))
-                            ]
-    else:
-        base_processors += [
-                            RProcessors.NormalizeBasedOnOther(guiding_keys=('pdos_array', 'pdos_array'),
-                                                              changing_keys=('fluence_array', 'pdos_array'),
-                                                              reference_method=('reduce_max', 'reduce_max'),
-                                                              changing_methods=('divide', 'divide'))
-                            ]
-    base_processors += [
-                      CProcessors.CombineKeys(axis=-1,
-                                              image_keys=input_keys,
-                                              output_key='test'),
-                      CProcessors.ReturnOutputs(input_keys=('test',),
-                                                output_keys=('fluence_array',))
+        CProcessors.Squeeze(image_keys=all_keys),
+        CProcessors.ExpandDimension(axis=-1, image_keys=all_keys),
+        CProcessors.MultiplyImagesByConstant(keys=drr_keys, values=(1/90, 1/90, 1/90, 1/90)),
+        CProcessors.CombineKeys(axis=-1,
+                                image_keys=input_keys,
+                                output_key='test'),
+        CProcessors.ReturnOutputs(input_keys=('test',),
+                                  output_keys=('fluence_array',)),
+        {'batch': batch}, {'repeat'}
     ]
-    base_processors += [
-                      #{'cache': train_cache},
-                      {'batch': batch}, {'repeat'}
-                      ]
     generator.compile_data_set(image_processors=base_processors, debug=False)
     return generator
-
-
-def create_files_for_streamline(records_path):
-    out_path_numpy = os.path.join(records_path, 'NumpyFiles')
-    out_path_jpeg = os.path.join(records_path, 'JpegsNoNormalizationMultipleProj')
-    for fold in [0]:
-        train_path = os.path.join(records_path, 'Train', 'fold{}'.format(fold))
-        record_paths = [train_path]
-        if fold == 0:
-            train_path = os.path.join(records_path, 'TrainNoNormalizationMultipleProj')
-            record_paths = [os.path.join(train_path, 'phantom_valid'), os.path.join(train_path, 'phantom_train')]
-        # record_paths = [train_path]
-        train_generator = DataGeneratorClass(record_paths=record_paths)
-        all_keys = ('pdos_array', 'fluence_array', 'drr_array', '5cm_deep_array', 'iso_array', '5cm_shallow_array',
-                    'deep_to_panel_array', 'iso_to_panel_array', 'shallow_to_panel_array')
-        # ['pdos_array', 'drr_array', 'iso_array', '-5cm_array', '5cm_array']
-        processors = [
-            CProcessors.Squeeze(image_keys=all_keys),
-            CProcessors.MultiplyImagesByConstant(keys=('drr_array', 'iso_array', '5cm_deep_array', '5cm_shallow_array'),
-                                                 values=(1/1, 1/1, 1/1, 1/1)),#(1 / 325, 1 / 325, 1/325, 1/325)
-            CProcessors.MultiplyImagesByConstant(keys=('pdos_array', 'fluence_array'),
-                                                 values=(1, 1)),#(1 / 3.448, 1 / 2.226)
-            CProcessors.ExpandDimension(axis=-1, image_keys=all_keys),
-            CProcessors.CombineKeys(axis=-1, image_keys=all_keys, output_key='combined'),
-            CProcessors.ReturnOutputs(input_keys=('combined',), output_keys=('out_file_name',)),
-            {'batch': 1}, {'repeat'}
-        ]
-        train_generator.compile_data_set(image_processors=processors, debug=True)
-        iterator = iter(train_generator.data_set)
-        ratios = []
-        # for i in range(len(train_generator)):
-        #     x, y = next(iterator)
-        #     ratio = x[0][0, 125, 125, 2] / x[0][0, 125, 125, 3]
-        #     ratios.append(ratio)
-        #     if ratio < 1:
-        #         xxx = 1
-        for i in range(len(train_generator)):
-            x, y = next(iterator)
-            #output, resized_pdos = generator.predict(x)
-            numpy_array = x[0].numpy()
-            file_info = str(y[0][0]).split('b')[-1][1:].split('.tf')[0]
-            print(file_info)
-            if file_info.split('_')[0] == '12':
-                xxx = 1
-            for i in range(numpy_array.shape[-1]):
-                numpy_array[..., i] /= np.max(numpy_array[..., i])
-            numpy_array *= 255
-            np.save(os.path.join(out_path_numpy, "{}.npy".format(file_info)), numpy_array)
-            max_val = np.max(numpy_array[...,-1])
-
-            if max_val < 20:
-                print("{} max is {}".format(file_info, max_val))
-            out_array = np.zeros((256, 256 * numpy_array.shape[-1]))
-            for i in range(numpy_array.shape[-1]):
-                out_array[..., 256 * i:256 * (i + 1)] = numpy_array[..., i]
-            image = Image.fromarray(out_array.astype('uint8'))
-            image.save(os.path.join(out_path_jpeg, "{}.jpeg".format(file_info)))
-    return None
 
 
 def convlayerBMA(x, filters, size, apply_batchnorm=True):
@@ -246,64 +169,21 @@ def return_datasets(data_generators):
     return all_datasets
 
 
-def return_generator(records_paths):
-    generator = DataGeneratorClass(record_paths=records_paths)
-    all_keys = ('pdos_array', 'drr_array', 'half_drr_array', 'fluence_array')
-    base_processors = [
-        CProcessors.Squeeze(image_keys=all_keys),
-        CProcessors.ExpandDimension(axis=-1, image_keys=('pdos_array', 'drr_array', 'half_drr_array', 'fluence_array')),
-        # RProcessors.NormalizeBasedOnOther(guiding_keys=('pdos_array', 'pdos_array'),
-        #                                   changing_keys=('fluence_array', 'pdos_array'),
-        #                                   reference_method=('reduce_max', 'reduce_max'),
-        #                                   changing_methods=('divide', 'divide')),
-        CProcessors.MultiplyImagesByConstant(keys=('pdos_array', 'fluence_array',
-                                                   'drr_array', 'half_drr_array'), values=(1/3.448, 1/2.226,
-                                                                                           1/325, 1/175)),
-        CProcessors.CombineKeys(axis=-1, image_keys=('pdos_array', 'drr_array', 'half_drr_array'),
-                                output_key='combined'),
-        CProcessors.ReturnOutputs(input_keys=('combined',), output_keys=('fluence_array',)),
-        {'shuffle': len(generator) // 3},
-        {'batch': 1}, {'repeat'}
-        ]
-    generator.compile_data_set(image_processors=base_processors, debug=False)
-    values_pdos = []
-    values_fluence = []
-    values_drr = []
-    values_drr_half = []
-    iterator = iter(generator.data_set)
-    for _ in range(len(generator)):
-        x, y = next(iterator)
-        values_pdos.append(np.max(x[0][..., 0].numpy()))
-        values_drr.append(np.max(x[0][..., 1].numpy()))
-        values_drr_half.append(np.max(x[0][..., 2].numpy()))
-        values_fluence.append(np.max(y[0].numpy()))
-    xxx = 1
-    return generator
-
-
-def return_fold_datasets(data_generators, excluded_fold=5, batch_size=1):
+def return_fold_datasets(data_generators, batch_size=1):
     all_datasets = return_datasets(data_generators)
-    train_dataset = None
-    for i in data_generators.keys():
-        print(i)
-        dataset = all_datasets[i]
-        print(dataset)
-        if i == excluded_fold:
-            valid_dataset = dataset
-            valid_dataset = valid_dataset.shuffle(len(dataset))
-            valid_dataset = valid_dataset.batch(batch_size)
-        elif train_dataset is None:
-            train_dataset = dataset
-        else:
-            train_dataset = train_dataset.concatenate(dataset)
-    train_dataset.shuffle(len(train_dataset))
+
+    train_dataset = all_datasets['train']
+    train_dataset = train_dataset.shuffle(len(train_dataset))
     train_dataset = train_dataset.batch(batch_size)
+
+    valid_dataset = all_datasets['validation']
+    # valid_dataset = valid_dataset.shuffle(len(valid_dataset))
+    valid_dataset = valid_dataset.batch(1)
     return train_dataset, valid_dataset
 
 
 def main():
-    records_path = r'\\ad.ucsd.edu\ahs\radon\research\Bojechko\TFRecords'
-    create_files_for_streamline(records_path)
+
     return None
     data_generators = {}
     xxx = return_generator([r'\\ad.ucsd.edu\ahs\radon\research\Bojechko\TFRecords\TrainNoNormalization\fold{}'.format(i) for i in range(1,6)])
@@ -325,5 +205,4 @@ def main():
 
 
 if __name__ == '__main__':
-    train_generator = main()
     pass
